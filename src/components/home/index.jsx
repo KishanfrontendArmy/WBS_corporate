91c30f082c7b8d27ccf7021a22a3289b769c3443
FEAvansh <108047738+FEAvansh@users.noreply.github.com>
2022-07-12 15:02:01 +0530

    [ourevents added and ui changes]

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,83 +1,87 @@
-import React, { useState, useEffect, Suspense } from 'react';
-import './home.css';
-import WOW from 'wowjs';
-import Header from '../Header'
-import HeroBanner from '../herobanner'
+import React, { useState, useEffect, Suspense } from "react";
+import "./home.css";
+import WOW from "wowjs";
+import Header from "../Header";
+import HeroBanner from "../herobanner";
 
-const Trescon = React.lazy(() => import('../trescon'));
-const ConnectioningBusiness = React.lazy(() => import ('../connectioningbusiness'));
-const AdvisoryBoard = React.lazy(() => import('../advisoryboard'));
-const AttendeeSay = React.lazy(() => import('../attendeesay'));
-const WorkedWithUs = React.lazy(() => import('../workedwithus'));
-const JoinUs = React.lazy(() => import('../joinus'));
-const GlobalOffices = React.lazy(() => import('../globaloffices'));
-const Footer = React.lazy(() => import('../footer'));
+const Trescon = React.lazy(() => import("../trescon"));
+const ConnectioningBusiness = React.lazy(() =>
+  import("../connectioningbusiness")
+);
+const AdvisoryBoard = React.lazy(() => import("../advisoryboard"));
+const AttendeeSay = React.lazy(() => import("../attendeesay"));
+const WorkedWithUs = React.lazy(() => import("../workedwithus"));
+const OurEventsHosted = React.lazy(() => import("../oureventshosted"));
+const JoinUs = React.lazy(() => import("../joinus"));
+const GlobalOffices = React.lazy(() => import("../globaloffices"));
+const Footer = React.lazy(() => import("../footer"));
 
 const Home = () => {
-    useEffect(() => {
-        new WOW.WOW({
-            live: false
-        }).init();
-    })
-    const [visible, setVisible] = useState(false)
+  useEffect(() => {
+    new WOW.WOW({
+      live: false,
+    }).init();
+  });
+  const [visible, setVisible] = useState(false);
 
-    const toggleVisible = () => {
-      const scrolled = document.documentElement.scrollTop;
-      if (scrolled > 400) {
-        setVisible(true)
-      }
-      else if (scrolled <= 400) {
-        setVisible(false)
-      }
-    };
-  
-    const scrollToTop = () => {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-        /* you can also use 'auto' behaviour
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 400) {
+      setVisible(true);
+    } else if (scrolled <= 400) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+      /* you can also use 'auto' behaviour
            in place of 'smooth' */
-      });
-    };
-  
-    window.addEventListener('scroll', toggleVisible);
-    
-    return (
-        <>
-            <div style={{ display: visible ? 'block' : 'none' }}>
-                <button className="Top_icon" onClick={scrollToTop}>
-                    <img src="/images/bottom_up.svg" alt="" />
-                </button>
-            </div>
-            <Header />
-            <HeroBanner />
-            <Suspense fallback={<></>}>
-                <Trescon />
-            </Suspense>
-            <Suspense fallback={<></>}>
-                <ConnectioningBusiness />
-            </Suspense>
-            <Suspense fallback={<></>}>
-                <AdvisoryBoard />
-            </Suspense>
-            <Suspense fallback={<></>}>
-                <AttendeeSay />
-            </Suspense>
-            <Suspense fallback={<></>}>
-                <WorkedWithUs />
-            </Suspense>
-            <Suspense fallback={<></>}>
-                <JoinUs />
-            </Suspense>
-            <Suspense fallback={<></>}>
-                <GlobalOffices />
-            </Suspense>
-            <Suspense fallback={<></>}>
-                <Footer />
-            </Suspense>
-        </>
-    )
+    });
+  };
 
-}
+  window.addEventListener("scroll", toggleVisible);
+
+  return (
+    <>
+      <div style={{ display: visible ? "block" : "none" }}>
+        <button className="Top_icon" onClick={scrollToTop}>
+          <img src="/images/bottom_up.svg" alt="" />
+        </button>
+      </div>
+      <Header />
+      <HeroBanner />
+      <Suspense fallback={<></>}>
+        <Trescon />
+      </Suspense>
+      <Suspense fallback={<></>}>
+        <ConnectioningBusiness />
+      </Suspense>
+      <Suspense fallback={<></>}>
+        <AdvisoryBoard />
+      </Suspense>
+      <Suspense fallback={<></>}>
+        <AttendeeSay />
+      </Suspense>
+      <Suspense fallback={<></>}>
+        <WorkedWithUs />
+      </Suspense>
+      <Suspense fallback={<></>}>
+        <OurEventsHosted />
+      </Suspense>
+      <Suspense fallback={<></>}>
+        <JoinUs />
+      </Suspense>
+      <Suspense fallback={<></>}>
+        <GlobalOffices />
+      </Suspense>
+      <Suspense fallback={<></>}>
+        <Footer />
+      </Suspense>
+    </>
+  );
+};
 
 export default Home;
---
+++ src/components/oureventshosted/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./oureventshosted.css";
+
+const OurEventsHosted = () => {
+  return (
+    <section className="our_events_hosted bg-white-main" id="joinuslink">
+      <div className="container">
+        <div className="title_box wow fadeInUp">
+          <h2>OUR EVENTS ARE HOSTED IN THE FOLLOWING COUNTRUES</h2>
+        </div>
+        <div className="our_events_hosted_inr d-flex row">
+          <div className="our_events_hosted_box mx-auto text-center">
+            <img className="mx-auto map_image" src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default OurEventsHosted;
+
+
+
+// src="images/events-locations.jpeg"
